0743a4f8fd15ea2bdda549bcffc5211a7f3ebd65
gehan92 <36468544+gehan92@users.noreply.github.com>
2019-10-25 22:10:24 +0530

    initial commit

---
+++ src/Hello/Program1.java
@@ -0,0 +1,14 @@
+package Hello;
+
+public class Program1 {
+
+	public Program1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
